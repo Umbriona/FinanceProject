6be4e5846247de678d2c05b56108c37469c0d778
Umbriona <33811221+Umbriona@users.noreply.github.com>
2018-07-26 07:40:35 +0200

    start on regression

--- StockTrader/Parser.py
+++ StockTrader/Parser.py
@@ -10,16 +10,16 @@ def parse_open(file_path='E:\\Financial_data\\open\\Amd_open.txt'):
     plane_text = ''.join(c for c in plane_text if c not in 'xBM()%,')
     split_n_list = plane_text.split('\n')
 
-    data_tensor = np.zeros([20, len(split_n_list)-1])
+    data_tensor = np.zeros([21, len(split_n_list)-1])
     j = 0
-    for i in split_n_list:
+    for i in split_n_list[:len(split_n_list)-1]:
         temp = i.split(' ')
         while '' in temp:
             temp.remove('')
         n = 0
         length_data = len(temp)-1
         for k in temp:
-            if n != length_data-3 and n == length_data-1 and n == length_data:
+            if n != length_data-3 and n != length_data-1 and n != length_data:
                 data_tensor[n, j] = float(k)
             elif n == length_data-3:
                 if k == 'N/A':
@@ -30,14 +30,17 @@ def parse_open(file_path='E:\\Financial_data\\open\\Amd_open.txt'):
                 time_split = k.split(':')
                 data_tensor[n, j] = float(time_split[0]) * 3600 + float(time_split[1]) * 60 + float(time_split[2])
             else:
-                z = ''.join(c for c in k if c not in ':')
-                data_tensor[n, j] = float(z)
+                z = k.split(':')
+                # z = ''.join(c for c in k if c not in ':')
+                data_tensor[n, j] = float(z[0])
+                data_tensor[n+1, j] = float(z[1])
+                data_tensor[n+2, j] = float(z[2])
             n += 1
         j += 1
-    m = 0
-    while np.all(data_tensor[length_data - m, :] == 0):
-        data_tensor = np.delete(data_tensor, length_data - m, axis=0)
-        m += 1
+
+    #while try np.all(data_tensor[length_data+3, :] == 0):
+     #   data_tensor = np.delete(data_tensor, length_data+3, axis=0)
+
 
     return data_tensor
 # lastClose | open |  bid price | Bid volume | Ask price | Ask volume | Volume | Volume ave|
@@ -50,7 +53,7 @@ def parse_closed(file_path='E:\\Financial_data\\closed\\Amd_closed.txt'):
     plane_text = ''.join(c for c in plane_text if c not in 'xBM()%,')
     split_n_list = plane_text.split('\n')
 
-    data_tensor = np.zeros([17, len(split_n_list)-1])
+    data_tensor = np.zeros([19, len(split_n_list)-1])
     j = 0
     for i in split_n_list:
         temp = i.split(' ')
@@ -72,9 +75,7 @@ def parse_closed(file_path='E:\\Financial_data\\closed\\Amd_closed.txt'):
                 data_tensor[n, j] = float(z)
             n += 1
         j += 1
-    m = 0
-    while np.all(data_tensor[length_data - m, :] == 0):
-        np.delete(data_tensor, length_data - m, axis=0)
-    m += 1
+
     return data_tensor
 
+parse_open()
--- StockTrader/Regression_analysis.py
+++ StockTrader/Regression_analysis.py
@@ -19,11 +19,64 @@ class Regression(threading.Thread):
         stock_data = Parser.parse_open(self.path_open + self.name)
         stock_trade = stock_data[1, :]
         print(stock_data)
+        normalised_time = self.time_conversion(stock_data)
+        from sklearn import linear_model
+        >> > reg = linear_model.LinearRegression()
+        >> > reg.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
+        LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+        >> > reg.coef_
+        array([0.5, 0.5])
 
-    def time_conversion(self, stock_data, time_interval = 'week'):
-        length_stock_data = len(stock_data[0, :])
-        stock_time = stock_data[]
+    def time_conversion(self, stock_data, time_interval='week', number=1):
+        length_stock_data_0 = len(stock_data[:, 1])
+        length_stock_data_1 = len(stock_data[0, :])
+        stock_time = stock_data[length_stock_data_0 - 4, :]
+        stock_date_year = stock_data[length_stock_data_0 - 3, :]
+        stock_date_month = stock_data[length_stock_data_0 - 2, :]
+        stock_date_day = stock_data[length_stock_data_0 - 1, :]
+
         if time_interval == 'week':
+            month_days_arraay = np.array([31, 28, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31])
+            if self.is_leap_year(stock_date_year[length_stock_data_1-1]):
+                month_days_arraay[1] = 29
+            start_day = stock_date_day[length_stock_data_1-1]
+            start_month = stock_date_month[length_stock_data_1-2]
+            total_day = 7 * number
+            day = start_day
+            month = start_month
+            total_day_left = total_day
+            while day < total_day_left:
+                total_day_left = total_day_left - day
+                if month == 1:
+                    month = 12
+                else:
+                    month -= 1
+                day = month_days_arraay[month-1]
+            day = day - total_day_left
+        # elif time_interval == 'month':
+        condition = np.logical_and(np.equal(stock_data[length_stock_data_0 - 1, :], day),
+                                   np.equal(stock_data[length_stock_data_0 - 2, :], month))
+        index_day = np.amin(np.nonzero(condition))
+        normalised_time = np.zeros(length_stock_data_1-index_day)
+        while day <= np.amax(stock_date_day):
+            condition = np.logical_and(np.equal(stock_data[length_stock_data_0 - 1, :], day),
+                                       np.equal(stock_data[length_stock_data_0 - 2, :], month))
+            if np.any(condition):
+                index_day_min = np.amin(np.nonzero(condition))
+                index_day_max = np.amax(np.nonzero(condition))
+                min_time_day = stock_time[index_day_min]
+                max_time_day = stock_time[index_day_max]
+                start_indice = np.amin(np.where(normalised_time == 0))
+                end_indice = np.count_nonzero(condition) + start_indice
+                normalised_time[start_indice:end_indice] = (stock_time[condition] - min_time_day) /\
+                                                           (max_time_day - min_time_day) + np.amax(normalised_time) + 0.01
+            day += 1
+        return normalised_time
+
+    def is_leap_year(self, year):
+        """Determine whether a year is a leap year."""
+
+        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 def main():
